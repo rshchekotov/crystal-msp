99adeb06eb7afa1b4505bd50302bdf01ce9bfe4b
Roman Shchekotov (Doomie) <rom.shchekotov@gmail.com>
2023-04-09 09:51:57 +0200

    🍻 implement a vuln.
    Implemented a RCE for demo purposes.
    
    Will be removed in one of the subsequent commits.

--- src/main/kotlin/org/doomlabs/crystal/msp/util/ItemUtils.kt
+++ src/main/kotlin/org/doomlabs/crystal/msp/util/ItemUtils.kt
@@ -1,8 +1,13 @@
 package org.doomlabs.crystal.msp.util
 
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.inventory.ItemStack
 
+fun Component.text(): String {
+    return PlainTextComponentSerializer.plainText().serialize(this)
+}
+
 fun ItemStack.name(): String {
-    return PlainTextComponentSerializer.plainText().serialize(this.displayName())
+    return this.displayName().text()
 }
